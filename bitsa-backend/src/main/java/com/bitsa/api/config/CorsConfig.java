e401f142ba4ef8cbabe3b5b60b4c741b88c13e52
allancheruiyotk <allancheruiyot69@gmail.com>
2025-11-22 16:12:27 +0300

    Added frontend Url

--- bitsa-backend/src/main/java/com/bitsa/api/config/CorsConfig.java
+++ bitsa-backend/src/main/java/com/bitsa/api/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "https://bitsawebsite.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
