1f05215b91ca58d47daef3b2671cadfc81c3ecb5
allancheruiyotk <allancheruiyot69@gmail.com>
2025-11-22 21:35:55 +0300

    Modified CorConfig file

--- bitsa-backend/src/main/java/com/bitsa/api/config/CorsConfig.java
+++ bitsa-backend/src/main/java/com/bitsa/api/config/CorsConfig.java
@@ -13,7 +13,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "https://bitsawebsite.netlify.app"));
+        //configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "https://bitsawebsite.netlify.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://bitsawebsite.netlify.app"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
